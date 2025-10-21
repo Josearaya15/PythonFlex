f5309980f2e1a971f46b2056fe64108b02c8490f
José Araya <joseignacioaraya15@gmail.com>
2025-10-20 22:17:27 -0300

    Update view for 'lista_proyectos' path

--- proyecto_django/consultoria/consultoria_app/urls.py
+++ proyecto_django/consultoria/consultoria_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('contacto/', views.formulario_contacto, name='contacto'),
     path('DiagnosticoRapido/', views.formulario_diagnostico_rapido, name='DiagnosticoRapido'),
     path('AñadirProyecto/', views.formulario_añadir_trabajador, name='AñadirProyecto'),
-    path('lista_proyectos/',views.lista_proyectos, name='lista_proyectos')
+    path('lista_proyectos/',views.view_lista_proyectos, name='lista_proyectos')
 ]
+
